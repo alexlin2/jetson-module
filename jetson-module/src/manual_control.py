51494b0d87aa3f71ddc4c6f54fe3194de22e46f0
alexlin <alexlin2@uw.edu>
2020-09-19 03:06:32 -0700

    changed control method

--- jetson-module/src/manual_control.py
+++ jetson-module/src/manual_control.py
@@ -1,10 +1,93 @@
 #!/usr/bin/python3
 import rospy
-from geometry_msgs.msg import Twist
-from std_msgs.msg import String
+from std_msgs import String
+from mavros_msg import OverrideRCIn
 import time
 
+def map(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
 class ManualControl():
+
+    def __init__(self):
+        rospy.loginfo("Message Type: uint16[], starting control node")
+        rospy.init_node('control_vel')
+        
+        self._cmd_vel_msg = OverrideRCIn()
+        self._steer_cache = 0.0
+        self._throttle_cache = 0.0
+
+        self.rate = rospy.Rate(30)
+        self.pub_cmd_vel = rospy.Publisher('/mavros/rc/OverrideRCIn', OverrideRCIn, queue_size = 1)
+        self.keyboard_cmd = rospy.Subscriber('keyboard_teleop', String, self.send_command)
+
+        self._last_time_cmd_rcv = time.time()
+        rospy.loginfo("Initialization complete")
+
+    def send_command(self, msg):
+            self._last_time_cmd_rcv = time.time()
+            self._cmd_vel_msg.channels = [0,1500,0,1500,0,0,0,0]
+            steer_increment = 0.02
+            throttle_increment = 0.05
+
+            if 'w' in msg.data:
+                if self._throttle_cache > 0:
+                    self._throttle_cache = 0.0
+                else:
+                    self._throttle_cache -= throttle_increment
+            elif 's' in msg.data:
+                if self._throttle_cache < 0:
+                    self._throttle_cache = 0.0
+                else:
+                    self._throttle_cache -= throttle_increment
+            else: 
+                self._throttle_cache = 0.0
+            if 'a' in msg.data:
+                if self._steer_cache > 0:
+                    self._steer_cache = 0.0
+                else:
+                    self._steer_cache -= steer_increment
+            elif 'd' in msg.data:
+                if self._steer_cache < 0:
+                    self._steer_cache = 0.0
+                else:
+                    self._steer_cache += steer_increment
+            else:
+                self._steer_cache = 0.0
+
+            self._steer_cache = min(1.0, max(-1.0, self._steer_cache))
+            self._throttle_cache = min(1.0, max(-1.0, self._throttle_cache))
+            steering = map(self._steer_cache, -1.0, 1.0, 1000, 2000)
+            throttle = map(self._steer_cache, -1.0, 1.0, 1300, 1700)
+            self._cmd_vel_msg.channels[1] = throttle
+            self._cmd_vel_msg.channels[3] = steering
+
+            self.pub_cmd_vel.publish(self._cmd_vel_msg)
+            
+        def set_cmd_vel_to_zero():
+            self._cmd_vel_msg.channels = [0,1500,0,1500,0,0,0,0]
+            self.pub_cmd_vel.publish(self._cmd_vel_msg)
+            
+        def is_controller_connected(self):
+            return time.time() - self._last_time_cmd_rcv < 3
+
+        def run(self):
+
+            while not rospy.is_shutdown():
+                if not self.is_controller_connected():
+                    rospy.loginfo("controller disconnected")
+                    self.set_cmd_vel_to_zero()
+                self.rate.sleep()
+
+if __name__ == "__main__":
+    cmd = ManualControl()
+    cmd.run()
+    
+        
+
+
+
+'''class ManualControl():
     
     def __init__(self):
         rospy.loginfo("Message Type: Twist, starting control node")
@@ -28,21 +111,21 @@ class ManualControl():
         self._last_time_cmd_rcv = time.time()
 
         if 'w' in msg.data:
-            self._cmd_vel_msg.linear.x = 0.05
+            self._cmd_vel_msg.linear.x = 0.3
         elif 's' in msg.data:
-            self._cmd_vel_msg.linear.x = -0.05
+            self._cmd_vel_msg.linear.x = -0.3
         else: 
             self._cmd_vel_msg.linear.x = 0
         if 'a' in msg.data:
-            self._cmd_vel_msg.angular.z = 1.0
+            self._cmd_vel_msg.angular.z = 0.5
         elif 'd' in msg.data:
-            self._cmd_vel_msg.angular.z = -1.0
+            self._cmd_vel_msg.angular.z = -0.5
         else:
             self._cmd_vel_msg.angular.z = 0
 
         self.pub_cmd_vel.publish(self._cmd_vel_msg)
         
-    def set_cmd_vel_to_zero(self):
+    def set_cmd_vel_to_zero():
         self._cmd_vel_msg.linear.x = 0
         self._cmd_vel_msg.angular.z = 0
         self.pub_cmd_vel.publish(self._cmd_vel_msg)
@@ -63,3 +146,4 @@ if __name__ == "__main__":
     cmd.run()
 
 
+'''
